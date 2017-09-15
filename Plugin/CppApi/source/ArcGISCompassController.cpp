60b9fcf31d513b665986c33ff3fc397384effd23
Ryan Kim <RKim@esri.com>
2017-09-15 15:59:17 -0700

    Correct 2-space indent

--- Plugin/CppApi/source/ArcGISCompassController.cpp
+++ Plugin/CppApi/source/ArcGISCompassController.cpp
@@ -113,12 +113,12 @@ double ArcGISCompassController::heading() const
 
 bool ArcGISCompassController::autoHide() const
 {
-    return m_autoHide;
+  return m_autoHide;
 }
 
 QString ArcGISCompassController::toolName() const
 {
-    return "ArcGISCompass";
+  return "ArcGISCompass";
 }
 
 } // Toolkit
