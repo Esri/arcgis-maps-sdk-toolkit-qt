dee24a7fd53352059323c2295baca53aa0fe4d5e
Luke Smallwood <lsmallwood@esri.com>
2019-08-14 13:59:10 +0100

    NorthArrow.pri

---
+++ toolkitquick/NorthArrow/NorthArrowController.h
@@ -0,0 +1,40 @@
+#ifndef NORTHARROWCONTROLLER_H
+#define NORTHARROWCONTROLLER_H
+
+#include <QObject>
+
+namespace Esri
+  {
+  namespace ArcGISRuntime
+    {
+    class MapQuickView;
+    }
+  }
+
+class NorthArrowController : public QObject
+{
+  Q_PROPERTY(Esri::ArcGISRuntime::MapQuickView* mapView READ mapView WRITE setMapView NOTIFY mapViewChanged)
+  Q_PROPERTY(int rotation READ rotation NOTIFY rotationChanged)
+
+  Q_OBJECT
+  Q_DISABLE_COPY(NorthArrowController)
+
+public:
+  NorthArrowController(QObject* parent = nullptr);
+  ~NorthArrowController();
+
+signals:
+  void mapViewChanged();
+  void rotationChanged();
+
+private:
+  Esri::ArcGISRuntime::MapQuickView* mapView() const;
+  void setMapView(Esri::ArcGISRuntime::MapQuickView* mapView);
+
+  int rotation() const;
+
+  Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
+  int m_rotation = 0;
+};
+
+#endif // NORTHARROWCONTROLLER_H
