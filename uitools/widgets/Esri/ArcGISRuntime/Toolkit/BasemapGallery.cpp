1b6be4c567cbf81d8171055dedcb9067a4c38d00
Alessio Williams Gava <awilliams@esri.com>
2021-10-04 13:39:57 +0100

    added constructor with single widget parameter. added getter and setter for geomodel this should be used when setting up a basemapgallery obj with the single param widget constructor

--- uitools/widgets/Esri/ArcGISRuntime/Toolkit/BasemapGallery.cpp
+++ uitools/widgets/Esri/ArcGISRuntime/Toolkit/BasemapGallery.cpp
@@ -1,32 +1,49 @@
+/*******************************************************************************
+ *  Copyright 2012-2021 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
 #include "BasemapGallery.h"
+
 #include "ui_BasemapGallery.h"
-
-
-//Toolkit controllers
 
 namespace Esri {
 namespace ArcGISRuntime {
 namespace Toolkit {
-BasemapGallery::BasemapGallery(GeoModel* geomodel, QWidget *parent) :
-  QFrame(parent),
-  m_ui(new Ui::BasemapGallery), m_controller(new BasemapGalleryController(this))
-{
-  m_ui->setupUi(this);
-  //setting the controller basemap and geoview
-  m_controller->setCurrentBasemap(geomodel->basemap());
-  m_controller->setGeoModel(geomodel);
+  BasemapGallery::BasemapGallery(QWidget* parent) :
+    QFrame(parent),
+    m_ui(new Ui::BasemapGallery),
+    m_controller(new BasemapGalleryController(this))
+  {
+    m_ui->setupUi(this);
+    GenericListModel* model = m_controller->gallery();
+    m_ui->listView->setModel(model);
 
-  GenericListModel* model = m_controller->gallery();
-  m_ui->listView->setModel(model);
-  /*have to set the property names, so the controller will know how to match the properties from
-  basemapgalleryitem with the specific Qt::<namespace> invoked in the .data() from the View (ListView) obj*/
-  model->setDisplayPropertyName("name");
-  model->setDecorationPropertyName("thumbnail");
-  model->setTooltipPropertyName("tooltip");
-  //delegate
+    /*have to set the property names, so the controller will know how to match the properties from
+    basemapgalleryitem with the specific Qt::<namespace> invoked in the .data() from the View (ListView) obj*/
+    model->setDisplayPropertyName("name");
+    model->setDecorationPropertyName("thumbnail");
+    model->setTooltipPropertyName("tooltip");
 
-  //signal-slots
-  connect(m_ui->listView, &QListView::clicked, this, &BasemapGallery::clickedItem);
+    //signal-slots
+    connect(m_ui->listView, &QListView::clicked, this, &BasemapGallery::clickedItem);
+  }
+
+  BasemapGallery::BasemapGallery(GeoModel* geomodel, QWidget* parent) :
+    BasemapGallery(parent)
+  {
+    m_controller->setCurrentBasemap(geomodel->basemap());
+    m_controller->setGeoModel(geomodel);
   }
 
   BasemapGallery::~BasemapGallery()
@@ -34,19 +51,34 @@ BasemapGallery::BasemapGallery(GeoModel* geomodel, QWidget *parent) :
     delete m_ui;
   }
 
-  BasemapGalleryController* BasemapGallery::controller() const {
+  BasemapGalleryController* BasemapGallery::controller() const
+  {
     return m_controller;
+  }
+
+  void BasemapGallery::setGeoModel(GeoModel* geomodel)
+  {
+    if (geomodel != nullptr)
+    {
+      m_controller->setCurrentBasemap(geomodel->basemap());
+      m_controller->setGeoModel(geomodel);
+    }
+  }
+
+  GeoModel* BasemapGallery::getCurrentGeoModel()
+  {
+    return m_controller->geoModel();
   }
 
   /*slot used to receive the clicked event on the listview
    *
    **/
-  void BasemapGallery::clickedItem(const QModelIndex &index){
+  void BasemapGallery::clickedItem(const QModelIndex& index)
+  {
     QObject* qvar = m_controller->gallery()->element(index);
     BasemapGalleryItem* item = qobject_cast<BasemapGalleryItem*>(qvar);
     //setting the basemap calculated from the current index. this will also modify the geoview
     m_controller->setCurrentBasemap(item->basemap());
-
   }
 }
 }
--- uitools/widgets/Esri/ArcGISRuntime/Toolkit/BasemapGallery.h
+++ uitools/widgets/Esri/ArcGISRuntime/Toolkit/BasemapGallery.h
@@ -1,41 +1,58 @@
+/*******************************************************************************
+ *  Copyright 2012-2021 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
 #ifndef ESRI_ARCGISRUNTIME_BASEMAPGALLERY_H
 #define ESRI_ARCGISRUNTIME_BASEMAPGALLERY_H
 
-#include <QFrame>
 #include "Basemap.h"
 #include "BasemapGalleryController.h"
+
+#include <QFrame>
 namespace Ui {
-class BasemapGallery;
+  class BasemapGallery;
 }
 
 namespace Esri {
 namespace ArcGISRuntime {
 namespace Toolkit {
-class BasemapGalleryController;
+  class BasemapGalleryController;
 
-class BasemapGallery : public QFrame
-{
-  Q_OBJECT
+  class BasemapGallery : public QFrame
+  {
+    Q_OBJECT
 
-public:
-  explicit BasemapGallery(GeoModel* basemap, QWidget *parent = nullptr);
-  ~BasemapGallery();
+  public:
+    explicit BasemapGallery(QWidget* parent = nullptr);
+    explicit BasemapGallery(GeoModel* basemap, QWidget* parent = nullptr);
+    ~BasemapGallery();
 
-  BasemapGalleryController* controller() const;
+    BasemapGalleryController* controller() const;
+    void setGeoModel(GeoModel* geomodel);
+    GeoModel* getCurrentGeoModel();
 
-  //signals
-  void clickedItem(const QModelIndex &index);
+    //signals
+    void clickedItem(const QModelIndex& index);
 
-private:
-  BasemapGalleryController* m_controller = nullptr;
-  Ui::BasemapGallery *m_ui;
-};
-
+  private:
+    BasemapGalleryController* m_controller = nullptr;
+    Ui::BasemapGallery* m_ui;
+  };
 
 }
 }
 
 }
-
 
 #endif // ESRI_ARCGISRUNTIME_BASEMAPGALLERY_H
