5128ddd89a65fe7a4139117cd9d6f9746bb320c6
Alessio Williams Gava <awilliams@esri.com>
2021-10-28 16:24:55 +0100

    overview map

--- uitools/examples/shared/qml/tools.js
+++ uitools/examples/shared/qml/tools.js
@@ -1,34 +1,38 @@
 const TOOLS = [
   {
-    "name": "North arrow",
-    "url": "NorthArrowDemo.qml"
+    "name": "Authentication view",
+    "url": "AuthenticationViewDemo.qml"
   },
   {
     "name": "Basemap gallery",
     "url": "BasemapGalleryDemo.qml"
   },
   {
-    "name": "Coordinate conversion",
-    "url": "CoordinateConversionDemo.qml"
-  },
-  {
-    "name": "Authentication view",
-    "url": "AuthenticationViewDemo.qml"
-  },
-  {
-    "name": "Time slider",
-    "url": "TimeSliderDemo.qml"
-  },
-  {
     "name": "Callout",
     "url": "CalloutDemo.qml"
   },
   {
-   "name": "Popup View",
-   "url": "PopupViewDemo.qml"
+    "name": "Coordinate conversion",
+    "url": "CoordinateConversionDemo.qml"
   },
+  {
+    "name": "North arrow",
+    "url": "NorthArrowDemo.qml"
+  },
+  {
+    "name": "Overview Map",
+    "url": "OverviewMapDemo.qml"
+  },
+  {
+    "name": "Popup View",
+    "url": "PopupViewDemo.qml"
+    },
   {
     "name": "Search View",
     "url": "SearchViewDemo.qml"
+  },
+  {
+    "name": "Time slider",
+    "url": "TimeSliderDemo.qml"
   }
 ];
