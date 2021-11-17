64d5c6501dc4705461fd1920167e1df28640c7d9
Archibald Neil MacDonald <nmacdonald@esri.com>
2021-11-17 16:23:46 +0000

    Registering C++ Timeslider demo.

---
+++ uitools/examples/cpp_quick/src/demos/TimeSliderDemoModel.cpp
@@ -0,0 +1,14 @@
+#include "TimeSliderDemoModel.h"
+
+#include "ArcGISMapImageLayer.h"
+#include "Map.h"
+#include "Scene.h"
+
+TimeSliderDemoModel::TimeSliderDemoModel(QObject* parent) :
+  BaseDemoModel(parent)
+{
+}
+
+TimeSliderDemoModel::~TimeSliderDemoModel()
+{
+}
---
+++ uitools/examples/cpp_quick/src/demos/TimeSliderDemoModel.h
@@ -0,0 +1,18 @@
+#ifndef ARCGIS_RUNTIME_TOOLKIT_CPP_QUICK_DEMO_TIMESLIDERDEMOMODEL_H
+#define ARCGIS_RUNTIME_TOOLKIT_CPP_QUICK_DEMO_TIMESLIDERDEMOMODEL_H
+
+#include "BaseDemoModel.h"
+
+#include <QObject>
+#include <QQmlEngine>
+
+class TimeSliderDemoModel : public BaseDemoModel
+{
+  Q_OBJECT
+  QML_ELEMENT
+public:
+  TimeSliderDemoModel(QObject* parent = nullptr);
+  ~TimeSliderDemoModel() override;
+};
+
+#endif // ARCGIS_RUNTIME_TOOLKIT_CPP_QUICK_DEMO_TIMESLIDERDEMOMODEL_H
