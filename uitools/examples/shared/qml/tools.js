3bb875040e3e29abf688e212e5ff223818a34a60
Archibald Neil MacDonald <nmacdonald@esri.com>
2021-11-17 16:11:21 +0000

    Adding shared code between QML and C++ test apps.

---
+++ uitools/examples/shared/qml/tools.js
@@ -0,0 +1,40 @@
+const TOOLS = [
+  {
+    "name": "Popup View",
+    "url": "PopupViewDemo.qml"
+  },
+  {
+    "name": "Overview Map",
+    "url": "OverviewMapDemo.qml"
+  },
+  {
+    "name": "Basemap gallery",
+    "url": "BasemapGalleryDemo.qml"
+  },
+  {
+    "name": "Callout",
+    "url": "CalloutDemo.qml"
+  },
+  {
+    "name": "Coordinate conversion",
+    "url": "CoordinateConversionDemo.qml"
+  },
+  {
+    "name": "North arrow",
+    "url": "NorthArrowDemo.qml"
+  },
+
+  {
+    "name": "Search View",
+    "url": "SearchViewDemo.qml"
+  },
+  {
+    "name": "Time slider",
+    "url": "TimeSliderDemo.qml"
+  },
+  {
+    "name": "Authentication view",
+    "url": "AuthenticationViewDemo.qml",
+    "handlesOwnAuthentication" : true
+  }
+];
