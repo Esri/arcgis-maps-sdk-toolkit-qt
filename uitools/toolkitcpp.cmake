414183113b9f1d1e238c4a8d2ca5473f5d120c06
Brian Noble <bnoble@esri.com>
2024-04-29 23:26:02 -0700

    Put common needed files in common.cmake, make widgets cmake file, adjust toolkitcpp.cmake

--- uitools/toolkitcpp.cmake
+++ uitools/toolkitcpp.cmake
@@ -11,62 +11,26 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set(CPPPATH ${CMAKE_CURRENT_LIST_DIR}/cpp/Esri/ArcGISRuntime/Toolkit)
+include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
 
 set(REGISTERPATH ${CMAKE_CURRENT_LIST_DIR}/register/Esri/ArcGISRuntime/Toolkit)
 
-set(TOOLKITCPP_SOURCES
-    ${CPPPATH}/AuthenticationController.cpp
-    ${CPPPATH}/BasemapGalleryController.cpp
-    ${CPPPATH}/BasemapGalleryItem.cpp
-    ${CPPPATH}/BookmarksViewController.cpp
-    ${CPPPATH}/BookmarkListItem.cpp
-    ${CPPPATH}/CoordinateConversionConstants.cpp
-    ${CPPPATH}/CoordinateConversionController.cpp
-    ${CPPPATH}/CoordinateConversionOption.cpp
-    ${CPPPATH}/CoordinateConversionResult.cpp
-    ${CPPPATH}/CoordinateOptionDefaults.cpp
-    ${CPPPATH}/FloorFilterController.cpp
-    ${CPPPATH}/FloorFilterFacilityItem.cpp
-    ${CPPPATH}/FloorFilterLevelItem.cpp
-    ${CPPPATH}/FloorFilterSiteItem.cpp
-    ${CPPPATH}/LocatorSearchSource.cpp
-    ${CPPPATH}/Internal/BasemapGalleryImageProvider.cpp
-    ${CPPPATH}/Internal/GenericListModel.cpp
-    ${CPPPATH}/Internal/GenericTableProxyModel.cpp
-    ${CPPPATH}/Internal/MetaElement.cpp
-    ${CPPPATH}/NorthArrowController.cpp
-    ${CPPPATH}/OverviewMapController.cpp
-    ${CPPPATH}/PopupViewController.cpp
-    ${CPPPATH}/ScalebarController.cpp
-    ${CPPPATH}/SearchResult.cpp
-    ${CPPPATH}/SearchSourceInterface.cpp
-    ${CPPPATH}/SearchSuggestion.cpp
-    ${CPPPATH}/SearchViewController.cpp
-    ${CPPPATH}/SmartLocatorSearchSource.cpp
-    ${CPPPATH}/TimeSliderController.cpp
-    ${CPPPATH}/UtilityNetworkFunctionTraceResult.cpp
-    ${CPPPATH}/UtilityNetworkFunctionTraceResultsModel.cpp
-    ${CPPPATH}/UtilityNetworkListItem.cpp
-    ${CPPPATH}/UtilityNetworkTraceController.cpp
-    ${CPPPATH}/UtilityNetworkTraceStartingPoint.cpp
-    ${CPPPATH}/UtilityNetworkTraceStartingPointsModel.cpp
+set(REGISTER_SOURCES
     ${REGISTERPATH}/register.cpp
     ${REGISTERPATH}/internal/register_cpp.cpp)
 
-set(TOOLKITCPP_RESOURCES
+set(TOOLKIT_RESOURCES
     ${CMAKE_CURRENT_LIST_DIR}/images/esri_arcgisruntime_toolkit_images.qrc
     ${CMAKE_CURRENT_LIST_DIR}/import/Esri/ArcGISRuntime/Toolkit/esri_arcgisruntime_toolkit_view.qrc)
 
 target_include_directories(${PROJECT_NAME} PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/cpp
-    ${CPPPATH}
     ${CMAKE_CURRENT_LIST_DIR}/register
     ${REGISTERPATH})
 
 target_sources(${PROJECT_NAME} PRIVATE
-    ${TOOLKITCPP_SOURCES}
-    ${TOOLKITCPP_RESOURCES})
+    ${TOOLKITCOMMON_SOURCES}
+    ${REGISTER_SOURCES}
+    ${TOOLKIT_RESOURCES})
 
 find_package(Qt6 COMPONENTS REQUIRED QuickControls2 WebView Svg QuickLayouts)
 
