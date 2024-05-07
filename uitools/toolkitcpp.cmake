e6aa2b489aae664c122f4445ccd987de17f1daf6
Brian Noble <bnoble@esri.com>
2024-05-07 14:32:39 -0700

    PR Frank Miller

--- uitools/toolkitcpp.cmake
+++ uitools/toolkitcpp.cmake
@@ -11,48 +11,49 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
+function(setup_toolkit TARGET_NAME TOOLKIT_DIR)
+    set(TOOLKITUI_DIR ${TOOLKIT_DIR}/uitools)
+    set(CPPPATH ${TOOLKITUI_DIR}/cpp/Esri/ArcGISRuntime/Toolkit)
+    set(REGISTERPATH ${TOOLKITUI_DIR}/register/Esri/ArcGISRuntime/Toolkit)
 
-set(REGISTERPATH ${CMAKE_CURRENT_LIST_DIR}/register/Esri/ArcGISRuntime/Toolkit)
+    file(GLOB_RECURSE TOOLKITCOMMON_HEADERS "${CPPPATH}/*.h")
+    file(GLOB_RECURSE TOOLKITCOMMON_SOURCES "${CPPPATH}/*.cpp")
+    file(GLOB_RECURSE TOOLKITREGISTER_HEADERS "${REGISTERPATH}/*.h")
+    file(GLOB_RECURSE TOOLKITREGISTER_SOURCES "${REGISTERPATH}/*.cpp")
 
-set(TOOLKITREGISTER_HEADERS
-    ${REGISTERPATH}/register.h
-    ${REGISTERPATH}/internal/register_cpp.h)
+    set(TOOLKIT_RESOURCES
+        ${TOOLKITUI_DIR}/images/esri_arcgisruntime_toolkit_images.qrc
+        ${TOOLKITUI_DIR}/import/Esri/ArcGISRuntime/Toolkit/esri_arcgisruntime_toolkit_view.qrc)
 
-set(TOOLKITREGISTER_SOURCES
-    ${REGISTERPATH}/register.cpp
-    ${REGISTERPATH}/internal/register_cpp.cpp)
+    target_include_directories(${TARGET_NAME} PRIVATE
+        ${TOOLKITUI_DIR}/cpp
+        ${TOOLKITUI_DIR}/register
+        ${CPPPATH}
+        ${REGISTERPATH})
 
-set(TOOLKIT_RESOURCES
-    ${CMAKE_CURRENT_LIST_DIR}/images/esri_arcgisruntime_toolkit_images.qrc
-    ${CMAKE_CURRENT_LIST_DIR}/import/Esri/ArcGISRuntime/Toolkit/esri_arcgisruntime_toolkit_view.qrc)
+    target_sources(${TARGET_NAME} PRIVATE
+        ${TOOLKITCOMMON_HEADERS}
+        ${TOOLKITCOMMON_SOURCES}
+        ${TOOLKITREGISTER_HEADERS}
+        ${TOOLKITREGISTER_SOURCES}
+        ${TOOLKIT_RESOURCES})
 
-target_include_directories(${PROJECT_NAME} PRIVATE
-    ${CMAKE_CURRENT_LIST_DIR}/register
-    ${REGISTERPATH})
+    find_package(Qt6 COMPONENTS REQUIRED QuickControls2 WebView Svg QuickLayouts)
 
-target_sources(${PROJECT_NAME} PRIVATE
-    ${TOOLKITCOMMON_HEADERS}
-    ${TOOLKITCOMMON_SOURCES}
-    ${TOOLKITREGISTER_HEADERS}
-    ${TOOLKITREGISTER_SOURCES}
-    ${TOOLKIT_RESOURCES})
+    target_link_libraries(${TARGET_NAME} PRIVATE
+        Qt6::QuickControls2
+        Qt6::WebView
+        Qt6::Svg)
 
-find_package(Qt6 COMPONENTS REQUIRED QuickControls2 WebView Svg QuickLayouts)
+    if(IOS)
+        target_link_libraries(${TARGET_NAME} PRIVATE Qt6::qquicklayoutsplugin)
+    else()
+        target_link_libraries(${TARGET_NAME} PRIVATE Qt6::QuickLayouts)
+    endif()
 
-target_link_libraries(${PROJECT_NAME} PRIVATE
-    Qt6::QuickControls2
-    Qt6::WebView
-    Qt6::Svg)
+    list(APPEND QML_IMPORT_PATH ${TOOLKITUI_DIR}/import)
 
-if(IOS)
-    target_link_libraries(${PROJECT_NAME} PRIVATE Qt6::qquicklayoutsplugin)
-else()
-    target_link_libraries(${PROJECT_NAME} PRIVATE Qt6::QuickLayouts)
-endif()
+    set(QML_IMPORT_PATH ${QML_IMPORT_PATH} CACHE STRING "" FORCE)
 
-list(APPEND QML_IMPORT_PATH ${CMAKE_CURRENT_LIST_DIR}/import)
-
-set(QML_IMPORT_PATH ${QML_IMPORT_PATH} CACHE STRING "" FORCE)
-
-add_definitions(-DCPP_ARCGISRUNTIME_TOOLKIT)
+    add_definitions(-DCPP_ARCGISRUNTIME_TOOLKIT)
+endfunction()
--- uitools/toolkitwidgets.cmake
+++ uitools/toolkitwidgets.cmake
@@ -11,74 +11,44 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
+function(setup_tookitwidgets TARGET_NAME TOOLKIT_DIR)
+    set(TOOLKITUI_DIR ${TOOLKIT_DIR}/uitools)
+    set(CPPPATH ${TOOLKITUI_DIR}/cpp/Esri/ArcGISRuntime/Toolkit)
+    set(WIDGETPATH ${TOOLKITUI_DIR}/widgets/Esri/ArcGISRuntime/Toolkit)
 
-set(WIDGETPATH ${CMAKE_CURRENT_LIST_DIR}/widgets/Esri/ArcGISRuntime/Toolkit)
+    message(TOOLKITUI_DIR ${TOOLKITUI_DIR})
 
-set(TOOLKITWIDGET_HEADERS
-    ${WIDGETPATH}/AuthenticationView.h
-    ${WIDGETPATH}/BasemapGallery.h
-    ${WIDGETPATH}/BookmarksView.h
-    ${WIDGETPATH}/CoordinateConversion.h
-    ${WIDGETPATH}/FloorFilter.h
-    ${WIDGETPATH}/NorthArrow.h
-    ${WIDGETPATH}/OverviewMap.h
-    ${WIDGETPATH}/Internal/ClientCertificatePasswordDialog.h
-    ${WIDGETPATH}/Internal/ClientCertificateView.h
-    ${WIDGETPATH}/Internal/CoordinateEditDelegate.h
-    ${WIDGETPATH}/Internal/Flash.h
-    ${WIDGETPATH}/Internal/OAuth2View.h
-    ${WIDGETPATH}/Internal/SslHandshakeView.h
-    ${WIDGETPATH}/Internal/UserCredentialView.h)
+    file(GLOB_RECURSE TOOLKITCOMMON_HEADERS "${CPPPATH}/*.h")
+    file(GLOB_RECURSE TOOLKITCOMMON_SOURCES "${CPPPATH}/*.cpp")
+    file(GLOB_RECURSE TOOLKITWIDGET_HEADERS "${WIDGETPATH}/*.h")
+    file(GLOB_RECURSE TOOLKITWIDGET_SOURCES "${WIDGETPATH}/*.cpp")
+    file(GLOB_RECURSE TOOLKITWIDGET_FORMS "${WIDGETPATH}/*.ui")
 
-set(TOOLKITWIDGET_SOURCES
-    ${WIDGETPATH}/AuthenticationView.cpp
-    ${WIDGETPATH}/BasemapGallery.cpp
-    ${WIDGETPATH}/BookmarksView.cpp
-    ${WIDGETPATH}/CoordinateConversion.cpp
-    ${WIDGETPATH}/FloorFilter.cpp
-    ${WIDGETPATH}/NorthArrow.cpp
-    ${WIDGETPATH}/OverviewMap.cpp
-    ${WIDGETPATH}/Internal/ClientCertificatePasswordDialog.cpp
-    ${WIDGETPATH}/Internal/ClientCertificateView.cpp
-    ${WIDGETPATH}/Internal/CoordinateEditDelegate.cpp
-    ${WIDGETPATH}/Internal/Flash.cpp
-    ${WIDGETPATH}/Internal/OAuth2View.cpp
-    ${WIDGETPATH}/Internal/SslHandshakeView.cpp
-    ${WIDGETPATH}/Internal/UserCredentialView.cpp)
+    set(TOOLKITWIDGET_RESOURCES ${TOOLKITUI_DIR}/images/esri_arcgisruntime_toolkit_images.qrc)
 
-set(TOOLKITWIDGET_RESOURCES ${CMAKE_CURRENT_LIST_DIR}/images/esri_arcgisruntime_toolkit_images.qrc)
+    target_include_directories(${TARGET_NAME} PRIVATE
+        ${TOOLKITUI_DIR}/cpp
+        ${TOOLKITUI_DIR}/widgets
+        ${CPPPATH}
+        ${WIDGETPATH})
 
-set(TOOLKITWIDGET_FORMS
-    ${WIDGETPATH}/AuthenticationView.ui
-    ${WIDGETPATH}/BasemapGallery.ui
-    ${WIDGETPATH}/BookmarksView.ui
-    ${WIDGETPATH}/CoordinateConversion.ui
-    ${WIDGETPATH}/FloorFilter.ui
-    ${WIDGETPATH}/OverviewMap.ui
-    ${WIDGETPATH}/Internal/ClientCertificatePasswordDialog.ui
-    ${WIDGETPATH}/Internal/ClientCertificateView.ui
-    ${WIDGETPATH}/Internal/OAuth2View.ui
-    ${WIDGETPATH}/Internal/SslHandshakeView.ui
-    ${WIDGETPATH}/Internal/UserCredentialView.ui)
+    target_sources(${TARGET_NAME} PRIVATE
+        ${TOOLKITCOMMON_HEADERS}
+        ${TOOLKITCOMMON_SOURCES}
+        ${TOOLKITWIDGET_HEADERS}
+        ${TOOLKITWIDGET_SOURCES}
+        ${TOOLKITWIDGET_RESOURCES}
+        ${TOOLKITWIDGET_FORMS})
 
-target_include_directories(${PROJECT_NAME} PRIVATE 
-    ${CMAKE_CURRENT_LIST_DIR}/widgets
-    ${WIDGETPATH})
+    find_package(Qt6 COMPONENTS REQUIRED Widgets WebEngineWidgets Svg)
 
-target_sources(${PROJECT_NAME} PRIVATE
-    ${TOOLKITCOMMON_HEADERS}
-    ${TOOLKITCOMMON_SOURCES}
-    ${TOOLKITWIDGET_HEADERS}
-    ${TOOLKITWIDGET_SOURCES}
-    ${TOOLKITWIDGET_RESOURCES}
-    ${TOOLKITWIDGET_FORMS})
+    target_link_libraries(${TARGET_NAME} PRIVATE
+        Qt::Widgets
+        Qt::WebEngineWidgets
+        Qt::Svg)
 
-find_package(Qt6 COMPONENTS REQUIRED Widgets WebEngineWidgets Svg)
+    add_definitions(-DWIDGETS_ARCGISRUNTIME_TOOLKIT)
+endfunction()
 
-target_link_libraries(${PROJECT_NAME} PRIVATE 
-    Qt::Widgets 
-    Qt::WebEngineWidgets 
-    Qt::Svg)
 
-add_definitions(-DWIDGETS_ARCGISRUNTIME_TOOLKIT)
+
