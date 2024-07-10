f542d5f7e51471b19fa57f4c09dcea31c05f76f7
Guillaume Belz <gbelz@esri.com>
2024-07-10 13:39:46 -0700

    fix doc

--- augmented_reality/ArApi/source/ArcGISArViewInterface.cpp
+++ augmented_reality/ArApi/source/ArcGISArViewInterface.cpp
@@ -36,9 +36,8 @@ namespace Esri::ArcGISRuntime::Toolkit {
   \since Esri::ArcGISRuntime 100.6
   \brief Base class to impement AR scene view.
 
-  This class provides the AR features without using the C++ or QML APIs of the ArcGIS Maps SDK for Qt.
-  It is used as a base class to create two API-dependent classes: \l ArcGISArSceneView which uses the C++ API and
-  \l QmlArcGISArSceneView which uses the QML API.
+  This class provides the AR features without using the C++ API of the ArcGIS Maps SDK for Qt.
+  It is used as a base class to create the API-dependent classe: \l ArcGISArView which uses the C++ API.
  */
 
 /*!
