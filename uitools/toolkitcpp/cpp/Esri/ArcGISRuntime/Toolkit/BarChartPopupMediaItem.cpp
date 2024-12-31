a6d2c4d7004601d826b314c888f640d3a4dae08c
Jared Caccamo <jcaccamo@esri.com>
2024-12-31 15:17:16 -0800

    Adding BarChart class.

---
+++ uitools/toolkitcpp/cpp/Esri/ArcGISRuntime/Toolkit/BarChartPopupMediaItem.cpp
@@ -0,0 +1,115 @@
+/*******************************************************************************
+ *  Copyright 2012-2025 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
+#include "BarChartPopupMediaItem.h"
+
+// Qt headers
+#include <QBarSet>
+
+// Maps SDK headers
+#include <PopupMedia.h>
+#include <PopupMediaItem.h>
+#include <PopupMediaValue.h>
+
+namespace Esri::ArcGISRuntime::Toolkit {
+
+/*!
+  \class Esri::ArcGISRuntime::Toolkit::BarChartPopupMediaItem
+  \inmodule ArcGISRuntimeToolkit
+  \brief Represents the contents of a \c{MediaPopupElement::media} to be used for a BarSeries in QML.
+  */
+
+/*!
+  \brief Constructor
+  \list
+    \li \a popupMedia - The \l {Esri::ArcGISRuntime::PopupMedia} {PopupMedia} used to populate the view.
+    \li \a jsonColorArray - The JSON array containing the color information for each bar set.
+    \li \a parent - The optional parent QObject.
+  \endlist
+ */
+BarChartPopupMediaItem::BarChartPopupMediaItem(PopupMedia* popupMedia, const QJsonArray& jsonColorArray, QObject* parent)
+  : PopupMediaItem{popupMedia, parent},
+    m_jsonDoc{jsonColorArray}
+{
+}
+
+/*!
+  \brief Returns the list of \c QBarSet objects. You must take ownership of the returned QBarSets.
+ */
+QList<QBarSet*> BarChartPopupMediaItem::barSets()
+{
+  QList<QBarSet*> barSets;
+  auto mediaValue = popupMediaItem()->value();
+
+  for (int i = 0; i < mediaValue->data().length(); i++)
+  {
+    const auto label = mediaValue->labels().at(i);
+    const auto value = mediaValue->data().at(i).toReal();
+
+    if (value >= m_maxValue)
+      m_maxValue = value;
+
+    if (value <= m_minValue)
+      m_minValue = value;
+
+    const auto barset = new QBarSet(label, this);
+    barset->append(value);
+    if (!m_jsonDoc.isEmpty())
+    {
+      // color scheme is [Red, Green, Blue, Alpha]
+      auto color = QColor(m_jsonDoc[i][0].toInt(), m_jsonDoc[i][1].toInt(), m_jsonDoc[i][2].toInt(), m_jsonDoc[i][3].toInt());
+      barset->setColor(color);
+      barset->setBorderColor(color);
+    }
+    barSets.append(barset);
+  }
+  emit barChartPopupMediaItemChanged();
+  return barSets;
+}
+
+/*!
+  \brief Returns the maximum value of all QBarSets.
+ */
+qreal BarChartPopupMediaItem::maxValue() const
+{
+  return m_maxValue;
+}
+
+/*!
+  \brief Returns the minimum value of all QBarSets.
+ */
+qreal BarChartPopupMediaItem::minValue() const
+{
+  return m_minValue;
+}
+
+} // namespace Esri::ArcGISRuntime::Toolkit
+
+/*!
+  \fn void Esri::ArcGISRuntime::Toolkit::BarChartPopupMediaItem::barChartPopupMediaItemChanged()
+  \brief Signal emitted when the underlying \c PopupMedia changes.
+ */
+
+/*!
+  \property Esri::ArcGISRuntime::Toolkit::BarChartPopupMediaItem::barSets
+ */
+
+/*!
+  \property Esri::ArcGISRuntime::Toolkit::BarChartPopupMediaItem::maxValue
+ */
+
+/*!
+  \property Esri::ArcGISRuntime::Toolkit::BarChartPopupMediaItem::minValue
+ */
---
+++ uitools/toolkitcpp/cpp/Esri/ArcGISRuntime/Toolkit/BarChartPopupMediaItem.h
@@ -0,0 +1,61 @@
+/*******************************************************************************
+ *  Copyright 2012-2025 Esri
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ******************************************************************************/
+#ifndef ESRI_ARCGISRUNTIME_TOOLKIT_BARCHARTPOPUPMEDIAITEM_H
+#define ESRI_ARCGISRUNTIME_TOOLKIT_BARCHARTPOPUPMEDIAITEM_H
+
+// Toolkit headers
+#include "PopupMediaItem.h"
+
+// Qt headers
+#include <QJsonArray>
+#include <QObject>
+
+class QBarSet;
+
+namespace Esri::ArcGISRuntime {
+
+class PopupMedia;
+
+namespace Toolkit {
+
+class BarChartPopupMediaItem : public PopupMediaItem
+{
+  Q_OBJECT
+  Q_PROPERTY(QList<QBarSet*> barSets READ barSets NOTIFY barChartPopupMediaItemChanged)
+  Q_PROPERTY(qreal maxValue READ maxValue NOTIFY barChartPopupMediaItemChanged)
+  Q_PROPERTY(qreal minValue READ minValue NOTIFY barChartPopupMediaItemChanged)
+
+public:
+  explicit BarChartPopupMediaItem(PopupMedia* popupMedia, const QJsonArray& jsonColorArray, QObject* parent = nullptr);
+  ~BarChartPopupMediaItem() override = default;
+
+  QList<QBarSet*> barSets();
+  qreal maxValue() const;
+  qreal minValue() const;
+
+signals:
+  void barChartPopupMediaItemChanged();
+
+private:
+  QList<QBarSet*> m_barSets;
+  QJsonArray m_jsonDoc;
+  qreal m_maxValue = 0.0;
+  qreal m_minValue = 0.0;
+};
+
+} // Toolkit
+} // Esri::ArcGISRuntime
+#endif // ESRI_ARCGISRUNTIME_TOOLKIT_BARCHARTPOPUPMEDIAITEM_H
