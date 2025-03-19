5e12e34d6452c0154350a694dcecc65a2f497807
James Ballard <jballard@esri.com>
2025-03-19 12:57:35 -0700

    Pull all the new authentication pieces.

--- uitools/toolkitcpp/src/Esri/ArcGISRuntime/Toolkit/register.cpp
+++ uitools/toolkitcpp/src/Esri/ArcGISRuntime/Toolkit/register.cpp
@@ -18,7 +18,6 @@
 #include "register.h"
 
 // Toolkit includes
-#include "ArcGISAuthenticationController.h"
 #include "AuthenticationController.h"
 #include "BasemapGalleryController.h"
 #include "BasemapGalleryItem.h"
@@ -91,8 +90,6 @@ namespace Esri::ArcGISRuntime::Toolkit {
     constexpr int VERSION_MAJOR = 200;
     constexpr int VERSION_MINOR = 2;
 
-    QPointer<ArcGISAuthenticationController> s_arcGISAuthenticationController;
-
     /*
       \internal
       \brief This namespace is an implementation detail for how to register types with QML.
@@ -139,22 +136,6 @@ namespace Esri::ArcGISRuntime::Toolkit {
       struct Singleton_
       {
       };
-
-      template <class T>
-      void registerComponentImpl(CreationType::Singleton_, int majorVersion, int minorVersion, const char* name)
-      {
-        qmlRegisterSingletonType<T>(NAMESPACE, majorVersion, minorVersion, name,
-                                    [](QQmlEngine* qmlEngine, QJSEngine* jsEngine) -> QObject*
-                                    {
-                                      if (!s_arcGISAuthenticationController)
-                                      {
-                                        s_arcGISAuthenticationController = T::create(qmlEngine, jsEngine);
-                                      }
-                                      return s_arcGISAuthenticationController;
-                                    });
-      }
-
-      [[maybe_unused]] constexpr Singleton_ Singleton = Singleton_{};
     }
 
     /*
@@ -213,7 +194,6 @@ namespace Esri::ArcGISRuntime::Toolkit {
     appEngine.addImageProvider(BasemapGalleryImageProvider::PROVIDER_ID, BasemapGalleryImageProvider::instance());
     appEngine.addImportPath(ESRI_COM_PATH);
     registerModuleRevisions();
-    registerComponent<ArcGISAuthenticationController>(CreationType::Singleton);
     registerComponent<AuthenticationController>();
     registerComponent<BasemapGalleryController>();
     registerComponent<BasemapGalleryItem>();
