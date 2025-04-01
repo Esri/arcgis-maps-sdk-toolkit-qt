d869fbd28bb7b0e5f6ba3656936a182b6dfb02aa
Brian Noble bri12415 <bri12415@esri.com>
2025-04-01 13:03:07 -0700

    Disable deprecation warnings for PopupViewController.cpp

--- uitools/common/src/PopupViewController.cpp
+++ uitools/common/src/PopupViewController.cpp
@@ -14,6 +14,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  ******************************************************************************/
+#define QRT_DISABLE_DEPRECATED_WARNINGS
+
 #include "PopupViewController.h"
 
 // Qt headers
