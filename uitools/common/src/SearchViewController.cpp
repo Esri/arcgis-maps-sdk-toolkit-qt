6beb506be047d5ef42f20c4feac616ccebcc0ff8
Brian Noble <bnoble@esri.com>
2025-08-22 14:26:59 -0600

    Remove LocatorTask credential constructor usage

--- uitools/common/src/SearchViewController.cpp
+++ uitools/common/src/SearchViewController.cpp
@@ -1,3 +1,18 @@
+// COPYRIGHT 2025 ESRI
+// TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
+// Unpublished material - all rights reserved under the
+// Copyright Laws of the United States and applicable international
+// laws, treaties, and conventions.
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, 92373
+// USA
+//
+// email: contracts@esri.com
+/// \file SearchViewController.cpp
 
 /*******************************************************************************
  *  Copyright 2012-2021 Esri
@@ -122,7 +137,7 @@ namespace Esri::ArcGISRuntime::Toolkit {
       // Reset sources to default.
       {
         sources()->clear();
-        auto locatorTask = new LocatorTask(QUrl(DEFAULT_LOCATOR_URL), nullptr, nullptr);
+        auto locatorTask = new LocatorTask(QUrl(DEFAULT_LOCATOR_URL));
         auto searchSource = new SmartLocatorSearchSource(locatorTask, m_sources);
         locatorTask->setParent(searchSource);
         sources()->append(searchSource);
