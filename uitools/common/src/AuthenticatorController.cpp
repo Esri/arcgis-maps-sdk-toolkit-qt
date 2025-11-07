e741f7e55bca1a30ace7ca292d59f39ecc227ba8
Brian Noble <bnoble@esri.com>
2025-11-07 12:57:04 -0700

    Assign a parent to the created TokenCredential

--- uitools/common/src/AuthenticatorController.cpp
+++ uitools/common/src/AuthenticatorController.cpp
@@ -1,3 +1,19 @@
+// COPYRIGHT 2025 ESRI
+// TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
+// Unpublished material - all rights reserved under the
+// Copyright Laws of the United States and applicable international
+// laws, treaties, and conventions.
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, 92373
+// USA
+//
+// email: contracts@esri.com
+/// \file AuthenticatorController.cpp
+
 /*******************************************************************************
  *  Copyright 2012-2024 Esri
  *
@@ -265,7 +281,8 @@ void AuthenticatorController::continueWithUsernamePasswordArcGIS_(const QString&
   TokenCredential::createAsync(m_currentArcGISChallenge->requestUrl(),
                                username,
                                password,
-                               std::nullopt).then(this, [this](TokenCredential* credential)
+                               std::nullopt,
+                               this).then(this, [this](TokenCredential* credential)
   {
     m_currentArcGISChallenge->continueWithCredential(credential);
     m_currentArcGISChallenge.reset();
