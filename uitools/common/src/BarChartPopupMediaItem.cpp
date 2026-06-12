fb5d21f4e6952f24283e696678b140d4c3fd7213
Ajith CP <achalliyil@esri.com>
2026-06-12 15:15:29 -0700

    bypass commit hook

--- uitools/common/src/BarChartPopupMediaItem.cpp
+++ uitools/common/src/BarChartPopupMediaItem.cpp
@@ -1,19 +1,3 @@
-// COPYRIGHT 2026 ESRI
-// TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
-// Unpublished material - all rights reserved under the
-// Copyright Laws of the United States and applicable international
-// laws, treaties, and conventions.
-//
-// For additional information, contact:
-// Environmental Systems Research Institute, Inc.
-// Attn: Contracts and Legal Services Department
-// 380 New York Street
-// Redlands, California, 92373
-// USA
-//
-// email: contracts@esri.com
-/// \file BarChartPopupMediaItem.cpp
-
 /*******************************************************************************
  *  Copyright 2012-2025 Esri
  *
@@ -30,10 +14,6 @@
  *  limitations under the License.
  ******************************************************************************/
 
-// At the top of your .cpp file or before Qt includes
-//#if defined(__aarch64__) || defined(__ARM_ARCH)
-#include <arm_acle.h>
-//#endif
 #include "BarChartPopupMediaItem.h"
 
 // Qt headers
